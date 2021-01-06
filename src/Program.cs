b55a380d9a924b8ee8e0c6739f51ea1ab822ed38
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2021-01-06 12:14:03 +0200

    added basic project files

---
+++ src/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NetworkFlow
+{
+    class Program
+    {
+        #region Arc
+        class Arc
+        {
+            public class Edge
+            {
+                public int from, to, capacity, flow;
+                public Edge(int u, int v, int c, int f)
+                {
+                    from = u;
+                    to = v;
+                    capacity = c;
+                    flow = f;
+                }
+            }
+
+            public static void add_flow(int idx, int flow, ref List<Edge> edges)
+            {
+                edges[idx].flow += flow;
+                edges[idx ^ 1].flow -= flow;
+            }
+            public static void add_edge(int u, int v, int c, ref List<Edge> edges, ref List<int>[] graph)
+            {
+                Edge from_edge = new Edge(u, v, c, 0);
+                Edge to_edge = new Edge(v, u, 0, 0);
+                graph[u].Add(edges.Count);
+                edges.Add(from_edge);
+                graph[v].Add(edges.Count);
+                edges.Add(to_edge);
+            }
+        }
+        #endregion
+
+        #region Edmonds Karp
+        class EdmondsKarp
+        {
+
+
+            static List<int>[] graph;
+            static int[] parentsList;
+            static List<Arc.Edge> edges = new List<Arc.Edge>();
+
+            static void bfs(int startNode, int endNode)
+            {
+
+                parentsList = Enumerable.Repeat((int)-1, graph.Length).ToArray();
+
+                Queue<int> q = new Queue<int>();
+                q.Enqueue(startNode);
+
+                while (q.Count != 0)
+                {
+                    int currentNode = q.Dequeue();
+
+                    for (int i = 0; i < graph[currentNode].Count; i++)
+                    {
+                        int idx = graph[currentNode][i];
+                        Arc.Edge e = edges[idx];
+                        if (parentsList[e.to] == -1 && e.capacity > e.flow && e.to != startNode)
+                        {
+                            parentsList[e.to] = idx;
+                            if (e.to == endNode)
+                                return;
+                            q.Enqueue(e.to);
+                        }
+                    }
+                }
+            }
+
+            static int edmondsKarp(int startNode, int endNode)
+            {
+                int maxFlow = 0;
+                while (true)
+                {
+                    bfs(startNode, endNode);
+                    if (parentsList[endNode] == -1)
+                    {
+                        break;
+                    }
+                    int flow = int.MaxValue;
+                    for (int node = parentsList[endNode]; node != -1; node = parentsList[edges[node].from])
+                    {
+                        flow = Math.Min(flow, edges[node].capacity - edges[node].flow);
+                    }
+                    maxFlow += flow;
+                    int currentNode = parentsList[endNode];
+                    while (currentNode != -1)
+                    {
+                        Arc.add_flow(currentNode, flow, ref edges);
+                        currentNode = parentsList[edges[currentNode].from];
+                    }
+                }
+                return maxFlow;
+            }
+
+            public static void Test(int NumTestCases)
+            {
+                string path_running_time = "running_time/EdmonsKarp.txt";
+                List<string> running_times = new List<string>();
+                for (int test_case = 1; test_case <= NumTestCases; ++test_case)
+                {
+                    string path = "tests/" + test_case.ToString() + ".txt";
+                    FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+                    StreamReader sr = new StreamReader(file);
+                    string[] line = sr.ReadLine().Split(' ');
+                    int num_of_nodes = int.Parse(line[0]);
+                    int num_of_edges = int.Parse(line[1]);
+                    line = sr.ReadLine().Split(' ');
+                    int source = int.Parse(line[0]) - 1;
+                    int sink = int.Parse(line[1]) - 1;
+                    graph = new List<int>[num_of_nodes];
+                    edges = new List<Arc.Edge>();
+                    for (int j = 0; j < num_of_nodes; j++)
+                    {
+                        graph[j] = new List<int>();
+                    }
+                    for (int i = 0; i < num_of_edges; ++i)
+                    {
+                        line = sr.ReadLine().Split(' ');
+                        int from = int.Parse(line[0]) - 1, to = int.Parse(line[1]) - 1, capacity = int.Parse(line[2]);
+                        Arc.add_edge(from, to, capacity, ref edges, ref graph);
+                    }
+                    Stopwatch sw = Stopwatch.StartNew();
+                    int maxFlow = edmondsKarp(source, sink);
+                    sw.Stop();
+                    int model_answer = int.Parse(sr.ReadLine().Split(' ')[0]);
+                    if (maxFlow == model_answer)
+                    {
+                        Console.WriteLine("Test #" + test_case + " is sucessfull and took " + sw.ElapsedMilliseconds + " milliseconds");
+                        running_times.Add(num_of_nodes + "," + num_of_edges + "," + maxFlow + "," + sw.ElapsedMilliseconds);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong answer on test #" + test_case + ", the right one is " + model_answer);
+                    }
+                    file.Close();
+                }
+                File.WriteAllLines(path_running_time, running_times);
+            }
+        }
+        #endregion
+
+        #region Ford Fulkerson
+        class FordFloukerson
+        {
+            static List<int>[] graph = new List<int>[500];
+            static List<Arc.Edge> edges;
+            static int[] parent;
+            static bool DFS(ref List<int>[] graph, int node, int sink, int source)
+            {
+                if (node == sink)
+                    return true;
+
+                foreach (var i in graph[node])
+                {
+                    if (parent[edges[i].to] == -1 && edges[i].capacity > edges[i].flow && edges[i].to != source)
+                    {
+                        parent[edges[i].to] = i;
+                        if (DFS(ref graph, edges[i].to, sink, source)) return true;
+                    }
+                }
+                return false;
+            }
+
+            static int Fordfulkerson(List<int>[] graph, int n, int s, int t)
+            {
+
+                int max_flow = 0;
+
+                while (true)
+                {
+                    parent = new int[n];
+                    for (int i = 0; i < n; ++i)
+                        parent[i] = -1;
+                    DFS(ref graph, s, t, s);
+                    if (parent[t] == -1)
+                        break;
+
+                    int path_flow = int.MaxValue;
+
+                    for (int node = parent[t]; node != -1; node = parent[edges[node].from])
+                    {
+                        path_flow = Math.Min(path_flow, edges[node].capacity - edges[node].flow);
+                    }
+
+                    for (int node = parent[t]; node != -1; node = parent[edges[node].from])
+                    {
+                        Arc.add_flow(node, path_flow, ref edges);
+                    }
+                    max_flow += path_flow;
+                }
+
+                return max_flow;
+            }
+
+            public static void Test(int NumTestCases)
+            {
+                string path_running_time = "running_time/FordFlukerson.txt";
+                List<string> running_times = new List<string>();
+                for (int test_case = 1; test_case <= NumTestCases; ++test_case)
+                {
+                    edges = new List<Arc.Edge>();
+                    string path = "tests/" + test_case.ToString() + ".txt";
+                    FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+                    StreamReader sr = new StreamReader(file);
+                    string[] line = sr.ReadLine().Split(" ");
+                    int num_of_nodes = int.Parse(line[0]);
+                    for (int k = 0; k < num_of_nodes; k++)
+                    {
+                        graph[k] = new List<int>();
+                    }
+
+                    int num_of_edges = int.Parse(line[1]);
+                    line = sr.ReadLine().Split(" ");
+                    int source = int.Parse(line[0]) - 1;
+                    int sink = int.Parse(line[1]) - 1;
+                    for (int i = 0; i < num_of_edges; ++i)
+                    {
+                        line = sr.ReadLine().Split(" ");
+                        int u = int.Parse(line[0]) - 1, v = int.Parse(line[1]) - 1, c = int.Parse(line[2]);
+                        Arc.add_edge(u, v, c, ref edges, ref graph);
+                    }
+                    int model_answer = int.Parse(sr.ReadLine().Split(" ")[0]);
+                    Stopwatch sw = Stopwatch.StartNew();
+                    int max_flow = Fordfulkerson(graph, num_of_nodes, source, sink);
+                    sw.Stop();
+                    if (max_flow == model_answer)
+                    {
+                        Console.WriteLine("Test #" + test_case + " is sucessfull and took " + sw.ElapsedMilliseconds + " milliseconds");
+                        running_times.Add(num_of_nodes + "," + num_of_edges + "," + max_flow + "," + sw.ElapsedMilliseconds);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong answer on test #" + test_case + ", the right one is " + model_answer);
+                        break;
+                    }
+                }
+                File.WriteAllLines(path_running_time, running_times.ToArray());
+            }
+        }
+
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of test cases to run: ");
+            int test = int.Parse(Console.ReadLine());
+            Console.WriteLine("* * * * * * * * * * Edmonds Karp * * * * * * * * * *");
+            EdmondsKarp.Test(test);
+            Console.WriteLine("* * * * * * * * *  Ford Fulkerson  * * * * * * * * *");
+            FordFloukerson.Test(test);
+        }
+    }
+}
---
+++ src/generator.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <limits>
+#include "testlib.h"
+
+using namespace std;
+
+class Generator {
+    class FlowGraph {
+    public:
+        struct Edge {
+            int from, to, capacity, flow;
+        };
+
+    private:
+        /* List of all - forward and backward - edges */
+        vector<Edge> edges;
+
+        /* These adjacency lists store only indices of edges in the edges list */
+        vector<vector<size_t> > graph;
+
+    public:
+        explicit FlowGraph(size_t n): graph(n) {}
+
+        void add_edge(int from, int to, int capacity) {
+            /* Note that we first append a forward edge and then a backward edge,
+            * so all forward edges are stored at even indices (starting from 0),
+            * whereas backward edges are stored at odd indices in the list edges */
+            Edge forward_edge = {from, to, capacity, 0};
+            Edge backward_edge = {to, from, 0, 0};
+            graph[from].push_back(edges.size());
+            edges.push_back(forward_edge);
+            graph[to].push_back(edges.size());
+            edges.push_back(backward_edge);
+        }
+
+        size_t size() const {
+            return graph.size();
+        }
+
+        const vector<size_t>& get_ids(int from) const {
+            return graph[from];
+        }
+
+        const Edge& get_edge(size_t id) const {
+            return edges[id];
+        }
+
+        void add_flow(size_t id, int flow) {
+            /* To get a backward edge for a true forward edge (i.e id is even), we should get id + 1
+            * due to the described above scheme. On the other hand, when we have to get a "backward"
+            * edge for a backward edge (i.e. get a forward edge for backward - id is odd), id - 1
+            * should be taken.
+            *
+            * It turns out that id ^ 1 works for both cases. Think this through! */
+            edges[id].flow += flow;
+            edges[id ^ 1].flow -= flow;
+        }
+    };
+
+    vector <int> compute_Gf(const FlowGraph& graph, int s, int t) {
+        // This function performs BFS
+
+        vector<int> parent(graph.size(), -1);
+        queue<int> frontire;
+
+        frontire.push(s);
+
+        while (!frontire.empty()) {
+            int node = frontire.front();
+            frontire.pop();
+
+            for (auto id : graph.get_ids(node)) {
+                const FlowGraph::Edge& e = graph.get_edge(id);
+
+                if (parent[e.to] == -1 && e.capacity > e.flow && e.to != s) {
+                    parent[e.to] = id;
+                    frontire.push(e.to);
+                }
+            }
+        }
+        
+        return parent;
+    }
+
+    int max_flow(FlowGraph& graph, int from, int to) {
+        int flow = 0;
+        
+        vector<int> parent;
+
+        do {
+            int min = numeric_limits<int>::max();
+
+            parent = compute_Gf(graph, from, to);
+
+            if(parent[to] != -1) {
+                
+                for (int u = parent[to]; u != -1; u = parent[graph.get_edge(u).from]) 
+                    min = std::min(min, 
+                    graph.get_edge(u).capacity - graph.get_edge(u).flow);
+                
+                flow += min;
+
+                for (int u = parent[to]; u != -1; u = parent[graph.get_edge(u).from]) 
+                    graph.add_flow(u, min);
+            }
+
+        } while(parent[to] != -1);
+
+        return flow;
+    }
+public:
+    void run() {
+        int number_of_test_cases = 500;
+
+        for(
+            int test_case = 1; 
+            test_case <= number_of_test_cases;
+            ++test_case) {
+
+            /// test case file
+            string path = "tests/" + to_string(test_case) + ".txt";
+            ofstream file(path);
+            /// Number of nodes and number of edges respictively
+            int number_of_nodes = rnd.next(3, 500), number_of_edges = rnd.next(number_of_nodes, 50000);
+            
+            /// get different sources and sinks
+            int source;
+            int sink;
+            do {
+                source = rnd.next(1, number_of_nodes);
+                sink = rnd.next(1, number_of_nodes);
+            } while(source == sink);
+
+            /// make graph
+            FlowGraph graph = FlowGraph(number_of_nodes);
+            file << number_of_nodes << ' ' << number_of_edges << '\n';
+            file << source << ' ' << sink << '\n';
+            for(int i = 0; i < number_of_edges; ++i) {
+                int u = rnd.next(1, number_of_nodes);
+                int v = rnd.next(1, number_of_nodes);
+                int capacity_per_hour = rnd.next(1, int(1e5));
+                /// Road between edges u and v that can handle a certain capacity
+                file << u << ' ' << v << ' ' << capacity_per_hour << '\n';
+                graph.add_edge(u - 1, v - 1, capacity_per_hour);
+            }
+            file << max_flow(graph, source - 1, sink - 1) << '\n';
+            file.close();
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    bool all_files_exist = true;
+    std::cout << "Checking if files are present\n";
+    for(int file_number = 1; file_number <= 500; ++file_number) {
+        std::string path = "tests/" + std::to_string(file_number) + ".txt";
+        std::ifstream f(path);
+        if(!f.good()) {
+            all_files_exist = false;
+            break;
+        }
+        f.close();
+    }
+    int generator_result;
+    if(all_files_exist)
+        generator_result = 0;
+    else {
+        std::cout << "Test cases not present... Please wait for them to be geenrated.\n";
+        Generator gen = Generator();
+        gen.run();
+        std::cout << "Done generating test cases!!.\n\n";
+        //return 0;
+    }
+
+    std::cout << "Test cases are present...\nProceeeding to test the network flow algorithm...\n";
+    int network_flow_result = system("bin/NetworkFlow.exe");
+    if(network_flow_result == 0) {
+        std::cout << "Congratulations!!! Your program ran successfully without any exceptions.\n";
+        std::cout << "To view the running times for analysis please go to running_time folder.\n";
+    }
+    else 
+        std::cerr << "Network Flow program exited unexpectedly\n";
+
+    std::cout << "Press any button to exit...";
+    getchar();
+    return 0;
+} 
